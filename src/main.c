3b214ade9237cbfde26ed016ce53dfa12d09356a
v1j4y <vijay.gopal.c@gmail.com>
2022-11-14 22:20:30 +0100

    Send/Recv for master and slave.

--- src/main.c
+++ src/main.c
@@ -1,74 +1,100 @@
 #include <stdio.h>
-
 #include <mpi.h>
+#include <sys/time.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-    int rank, size;
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
-    MPI_Comm_size( MPI_COMM_WORLD, &size);
+int rank, size;
+MPI_Status status;
 
-    int left_rank = (rank==0)?(size-1):(rank-1);
-    int right_rank = (rank==(size-1))?0:(rank+1)%size;
+/* Init */
+MPI_Init(&argc, &argv);
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int stop_cond_rank;
-    MPI_Request stop_cond_request;
-    int stop_cond= 0;
+if (rank != 0) { // Slaves
+    int buf;
 
-    int i, s=0;
-    int smax = 1 << 20 + 10 * rank;
-    printf( " -- %d -- smax = %d \n", rank, smax);
+    if (rank == 1) {
+        buf = 1;
+        MPI_Send(&buf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+    if (rank == 1) {
+        buf = 1;
+        MPI_Send(&buf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+    if (rank == 2) {
+        buf = 2;
+        MPI_Send(&buf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+    if (rank == 2) {
+        buf = 2;
+        MPI_Send(&buf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
 
-    while( s <= (1 << 22) )
-    {
-
-        /* Compute Here and set stop condition accordingly */
-        for ( i = 0; i < (1 << 10); ++i )
-            s += 1;
-
-        if (s >= (1 << smax ))
-            stop_cond = 1;
-
-        if( stop_cond )
+    int sum = 0;
+    int flag = -1, res;
+    MPI_Request request;
+    MPI_Status status;
+    while (1) {
+        if(flag != 0)
         {
-            /* Cancel the left recv */
-            MPI_Cancel( &stop_cond_request );
-            if( rank != right_rank )
-                MPI_Send( &rank, 1, MPI_INT, right_rank, 123, MPI_COMM_WORLD );
-
-            break;
+            MPI_Irecv(&res, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
+            flag = 0;
         }
 
-        int did_recv = 0;
-        MPI_Test( &stop_cond_request, &did_recv, MPI_STATUS_IGNORE );
-        if( did_recv )
-        {
+        MPI_Test(&request, &flag, &status);
 
-            printf(" ---- DID RECV ---- \n");
-
-            MPI_Irecv( &stop_cond_rank, 1, MPI_INT, left_rank, 123, MPI_COMM_WORLD, &stop_cond_request);
-
-            stop_cond = 1;
-            MPI_Wait( &stop_cond_request, MPI_STATUS_IGNORE );
-            if( right_rank != stop_cond_rank )
-                MPI_Send( &stop_cond_rank, 1, MPI_INT, right_rank, 123, MPI_COMM_WORLD );
-
-            break;
+        if (flag != 0) {
+            printf("recv : %d, slave : %d\n", res, status.MPI_SOURCE);
+            if (status.MPI_SOURCE != -1)
+                sum += res;
+            flag = -1;
         }
+
+
+        if (sum == 0)
+            break;
     }
 
-    if( stop_cond )
-    {
-        /* Handle the stop condition */
-        printf(" ---- Done ---- \n");
-    }
-    else
-    {
-        /* Cleanup */
-        MPI_Cancel( &stop_cond_request );
+    printf("rank = %d sum : %d\n", rank, sum);
+}
+else { // Master
+    int sum = 0;
+    int flag = -1, res;
+    MPI_Request request;
+    MPI_Status status;
+    while (1) {
+        if(flag != 0)
+        {
+            MPI_Irecv(&res, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
+            flag = 0;
+        }
+
+        MPI_Test(&request, &flag, &status);
+
+        if (flag != 0) {
+            printf("recv : %d, slave : %d\n", res, status.MPI_SOURCE);
+            if (status.MPI_SOURCE != -1)
+                sum += res;
+            flag = -1;
+        }
+
+
+        if (sum == 6)
+            break;
     }
 
-    MPI_Finalize();
-    return(0);
+    int buf;
+    buf=0;
+    MPI_Send(&buf, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    buf=0;
+    MPI_Send(&buf, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+    printf("sum : %d\n", sum);
+}
+
+MPI_Finalize();
+return 0;
+
 }
